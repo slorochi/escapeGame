4d61b22da0b07f24d5472d7f3e99c5429777a751
Gaetan-Tcode <82641133+Gaetan-Tcode@users.noreply.github.com>
2022-02-19 13:19:56 +0100

    Création des dossier mvc

--- index.php
+++ index.php
@@ -4,13 +4,11 @@ require("./core/rooter.php");
 ?>
 <!doctype html>
 <html>
+<head>
     <head>
-        <head>
-            <meta charset="UTF-8">
-        </head>
-    <body>
-<?php 
-    echo $content;
-?>    
-    </body>
+        <meta charset="UTF-8">
+    </head>
+<body>
+    <?php echo $content; ?>    
+</body>
 </html>
