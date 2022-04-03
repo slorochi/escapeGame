51d7ce4f15190e045c2e3538d149103576ce93de
Gaetan-Tcode <teixeira.gaetan@outlook.fr>
2022-04-03 14:42:35 +0200

    Update BddConnection.php

--- src/models/BddConnection.php
+++ src/models/BddConnection.php
@@ -74,10 +74,10 @@ class BddConnection{
 
 	public function createEscape(string $table, $elementToCreate){
 		$db = $this->getconnect();
-		$sql = "INSERT INTO $table SET idEscape = :idEscape, nom = :nom, niveau = :niveau, idType = :idType, adresse = :adresse, cp = :cp, ville = :ville" ;
+		$sql = "INSERT INTO $table SET idEscape = :idEscape, nom = :nom, niveau = :niveau, idType = :idType, adresse = :adresse, cp = :cp, ville = :ville, description = :description" ;
 		$rst = $db->prepare($sql);
 		$rst->execute(
-			[":idEscape" => $elementToCreate->getIdEscape(), ":nom" => $elementToCreate->getNom(), ":niveau" => $elementToCreate->getNiveau(), ":idType" => $elementToCreate->getIdType(), ":adresse" => $elementToCreate->getAdresse(), ":cp" => $elementToCreate->getCp(), ":ville" => $elementToCreate->getVille()]);
+			[":idEscape" => $elementToCreate->getIdEscape(), ":nom" => $elementToCreate->getNom(), ":niveau" => $elementToCreate->getNiveau(), ":idType" => $elementToCreate->getIdType(), ":adresse" => $elementToCreate->getAdresse(), ":cp" => $elementToCreate->getCp(), ":ville" => $elementToCreate->getVille(), "descrpition" => $elementToCreate->getDescription()]);
 		return  $rst->fetchAll(PDO::FETCH_ASSOC); 
 
 	}
