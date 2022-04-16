1a61d2b77c1abdac94bbe67dc4fa866a99b045c1
Gaetan-Tcode <teixeira.gaetan@outlook.fr>
2022-04-16 11:13:55 +0200

    HTML

--- public/views/accueil.php
+++ public/views/accueil.php
@@ -2,13 +2,20 @@
     <div class="container">
         <div class="row">
             <div class="col-4 image">
-                <img src="views/style/img/be4be3e0-5dae-11ec-bfae-50d2ca6eaeba.jfif" alt="">
+                <img src="views/style/img/4129209.jpg" alt="">
             </div>
-            <div class="col-8">
-                <h1>Bienvenue chez LEVEL GAME</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima iusto temporibus esse eius ullam.
-                </p>
+            <div class="col-8 text">
+                <div>
+                    <h1>Bienvenue chez LEVEL GAME</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda vero vel porro sunt iste eos ullam ipsum autem velit veritatis qui voluptatum blanditiis, quia labore in error sit excepturi, possimus doloribus officiis. Quas quaerat qui pariatur obcaecati? Minus, quos cumque!
+                    </p>
+                </div>
+                
             </div>
         </div>
     </div>
+</section>
+<section>
+    
 </section>
