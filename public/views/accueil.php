1f5babf7da39b0799c1f83e2b9e80aebebf339c1
Gaetan-Tcode <teixeira.gaetan@outlook.fr>
2022-04-16 12:06:52 +0200

    mise a jour html

--- public/views/accueil.php
+++ public/views/accueil.php
@@ -1,4 +1,4 @@
-<section id="quisommenous" >
+<section class="carteAccueil" >
     <div class="container">
         <div class="row">
             <div class="col-4 image">
@@ -10,12 +10,30 @@
                     <p>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda vero vel porro sunt iste eos ullam ipsum autem velit veritatis qui voluptatum blanditiis, quia labore in error sit excepturi, possimus doloribus officiis. Quas quaerat qui pariatur obcaecati? Minus, quos cumque!
                     </p>
+                    <a href="?p=catalogue">VOIR NOS ESCAPES GAMES</a>
                 </div>
                 
             </div>
         </div>
     </div>
 </section>
-<section>
-    
+<section class="carteAccueil">
+    <div class="container">
+        <div class="row">
+            <div class="col-8 text">
+                <div>
+                    <h2>Le top des meilleurs</h2>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda vero vel porro sunt iste eos ullam ipsum autem velit veritatis qui voluptatum blanditiis, quia labore in error sit excepturi, possimus doloribus officiis. Quas quaerat qui pariatur obcaecati? Minus, quos cumque!
+                    </p>
+                    <a href="?p=leaderboard">VOIR LE LEADERBOARD</a>
+                </div>
+                
+            </div>
+            <div class="col-4 image">
+                <img src="views/style/img/un-crime-presque-parfait-escape-game-lock-academy.jpg" alt="">
+            </div>
+            
+        </div>
+    </div>
 </section>
--- public/views/escapegame.php
+++ public/views/escapegame.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="row">
         <div class="card mb-3">
-            <img src="views/style/img/be4be3e0-5dae-11ec-bfae-50d2ca6eaeba.jfif" class="card-img-top" alt="...">
+            <img src="views/style/img/escgame<?= $idType ?>.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title"><?= $nom ?></h5>
                 <p class="card-text"><?= $description ?></p>
